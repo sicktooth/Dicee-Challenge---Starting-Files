908903cd4b9410e8e0bf7ed95b52f08bdb893574
sicktooth <sosaronungwe@gmail.com>
2024-02-03 09:01:13 +0100

    updated index.js

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ if (randomNumber1 > randomNumber2) {
 else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").textContent = "Player 2 wins! 🚩";
 }
-else if (randomNumber1 === randomNumber2) {
+else {
     document.querySelector("h1").textContent = "Draw!";
 }
