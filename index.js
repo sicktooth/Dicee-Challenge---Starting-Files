8cf474136b5a73b19815c0be9360d3e8b623be4c
sicktooth <sosaronungwe@gmail.com>
2024-02-03 08:58:20 +0100

    fixed it

--- index.js
+++ index.js
@@ -1,63 +1,21 @@
-var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 * 6;
-randomNumber1 = Math.floor(randomNumber1) + 1;
-console.log(randomNumber1);
-if (randomNumber1 === 1) {
-    document.querySelector("img.img1").setAttribute("src","/images/dice1.png");
-} 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// console.log(randomNumber1);
 
-else if (randomNumber1 === 2) {
-    document.querySelector("img.img1").setAttribute("src","/images/dice2.png");
-} 
+var imageSource = "/images/dice" + randomNumber1 + ".png";
 
-else if (randomNumber1 === 3) {
-    document.querySelector("img.img1").setAttribute("src","/images/dice3.png");
-} 
+var img1 = document.querySelectorAll("img")[0].setAttribute("src", imageSource);
 
-else if (randomNumber1 === 4) {
-    document.querySelector("img.img1").setAttribute("src","/images/dice4.png");
-} 
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-else if (randomNumber1 === 5) {
-    document.querySelector("img.img1").setAttribute("src","/images/dice5.png");
-} 
+var imageSource0 = "/images/dice" + randomNumber2 + ".png";
 
-else if (randomNumber1 === 6) {
-    document.querySelector("img.img1").setAttribute("src","/images/dice6.png");
-}
-
-var randomNumber2 = Math.random() * 6;
-randomNumber2 = Math.floor(randomNumber2) + 1;
-console.log(randomNumber2);
-if (randomNumber2 === 1) {
-    document.querySelector("img.img1").setAttribute("src","/images/dice1.png");
-} 
-
-else if (randomNumber2 === 2) {
-    document.querySelector("img.img2").setAttribute("src","/images/dice2.png");
-} 
-
-else if (randomNumber2 === 3) {
-    document.querySelector("img.img2").setAttribute("src","/images/dice3.png");
-} 
-
-else if (randomNumber2 === 4) {
-    document.querySelector("img.img2").setAttribute("src","/images/dice4.png");
-} 
-
-else if (randomNumber2 === 5) {
-    document.querySelector("img.img2").setAttribute("src","/images/dice5.png");
-} 
-
-else if (randomNumber2 === 6) {
-    document.querySelector("img.img2").setAttribute("src","/images/dice6.png");
-}
+var img2 = document.querySelectorAll("img")[1].setAttribute("src", imageSource0);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "🚩 Player 1 wins";
+    document.querySelector("h1").textContent = "🚩 Player 1 wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").textContent = "Player 2 wins 🚩";
+    document.querySelector("h1").textContent = "Player 2 wins! 🚩";
 }
 else if (randomNumber1 === randomNumber2) {
     document.querySelector("h1").textContent = "Draw!";
